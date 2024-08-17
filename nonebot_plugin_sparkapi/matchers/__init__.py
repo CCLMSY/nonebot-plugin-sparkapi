d8420e4ae8c18258abd2596e5509b8ae28a357e5
wyf7685 <wyf7685@163.com>
2024-08-17 21:27:01 +0800

    make ruff happy

--- nonebot_plugin_sparkapi/matchers/__init__.py
+++ nonebot_plugin_sparkapi/matchers/__init__.py
@@ -1,1 +1,12 @@
 from . import block_private, chat, clear, help, imggen, pptgen, preset, session
+
+__all__ = [
+    "block_private",
+    "chat",
+    "clear",
+    "help",
+    "imggen",
+    "pptgen",
+    "preset",
+    "session",
+]
--- nonebot_plugin_sparkapi/matchers/session/__init__.py
+++ nonebot_plugin_sparkapi/matchers/session/__init__.py
@@ -1,1 +1,3 @@
 from . import delete, load, save, session, show
+
+__all__ = ["delete", "load", "save", "session", "show"]
